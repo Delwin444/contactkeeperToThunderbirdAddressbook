4954fb07d5eaa51a4eb1ec9d16b5d78ee3d873ca
Delwin444 <justinkuhn@t-online.de>
2023-12-11 01:17:01 +0100

    Refactoring file reading and error handling

--- src/convert.js
+++ src/convert.js
@@ -1,25 +1,45 @@
-let convertData = function (data) {
+function convertData (data) {
 
-};
+}
 
 
-document.getElementById('fileInput').addEventListener('change', function(e) {
+/**
+ * Converts the file data using the convertData method.
+ *
+ * @return {void} - Returns nothing.
+ */
+function processData() {
+    convertData(this.result);
+}
+
+/**
+ * Checks if a single file is selected by the user.
+ *
+ * @param {Event} e - The event object representing the file selection.
+ *
+ * @return {void}
+ */
+function processSingleFile(e) {
     if (e.target.files.length !== 1) {
         window.alert('Please select one File for import.')
+        return
     }
 
     let file = e.target.files[0];
+    readAndProcessFile(file);
+}
+
+/**
+ * Reads a file using FileReader and processes it.
+ *
+ * @param {File} file - The file to be processed.
+ *
+ * @return {void}
+ */
+function readAndProcessFile(file) {
     let reader = new FileReader();
-
-    reader.onload = (function() {
-        return function(e) {
-            document.getElementById('fileContent').innerText = e.target.result;
-        };
-    })(file);
-
-    reader.onLoadEnd = function () {
-        convertData(reader.result);
-    }
-
+    reader.onloadend = processData;
     reader.readAsText(file);
-});
+}
+
+document.getElementById('fileInput').addEventListener('change', processSingleFile);
